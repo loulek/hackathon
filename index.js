cc5cc15f9f85560928ebd1b2170ee3d2fcd7b5ff
Mojiashen1 <mojiashen1@gmail.com>
2016-07-09 21:27:44 -0400

    test 7

--- index.js
+++ index.js
@@ -31,12 +31,10 @@ app.post('/webhook/', function(req, res){
   for (let i = 0; i < messaging_events.length; i ++) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
+    console.log(event.sender.id)
     if (event.message && event.message.text) {
       // let text = event.message.text
-      sendTextMessage(sender, "Hello there, I am Pam, your personal assistant. Let's set you up")
-      sendTextMessage(sender, "I'll help you get up in the mornings and fulfill your personal goals")
-      sendTextMessage(sender, "To start, what time do you usually wake up?")//recieve text back
-      // sendTextMessage(sender, "Sweet")
+      sendTextMessage(sender, ["Hello there, I am Pam, your personal assistant. Let's set you up", "I'll help you get up in the mornings and fulfill your personal goals", "To start, what time do you usually wake up?"])
       resToMorningRoutine(sender)
 
       // console.log('payload', )
@@ -47,8 +45,7 @@ app.post('/webhook/', function(req, res){
       console.log("EVENT POSTBACK ", event.postback)
       let text = event.postback.payload
       if (text === 'yes') {
-        sendTextMessage(sender, "Meditation, pushups, tea? What's one thing you should you be doing every morning?")
-        sendTextMessage(sender, "For example, you could respond 'Meditation for 10 minutes', or... 'Read for 20 minutes'?")
+        sendTextMessage(sender, ["Meditation, pushups, tea? What's one thing you should you be doing every morning?", "For example, you could respond 'Meditation for 10 minutes', or... 'Read for 20 minutes'?"])
         continue
       } else if (text === 'no') {
         sendTextMessage(sender, "options.hat's one thing you should you be doing every morning?")
@@ -61,23 +58,27 @@ app.post('/webhook/', function(req, res){
 
 const token = "EAACGElIklxMBAGeo6OyZBOOPCudxZCun6dF7noz5P3HixXIzeZClJNDkzQLAFZCyl61Thn98jXzuNo6bE85ZBaxmK5bBmutKFR9O9mLuFJl5h6NHl55L1EmslH6u53IbKtLYTqj2FPmIojrv2wpJ0odaS7ZCh5RUY0XXGymp3qxQZDZD"
 
-function sendTextMessage(sender, text) {
-  let messageData = {text: text}
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: token},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: messageData
-    }
-  }, function(error, response,body) {
-    if (error) {
-      console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
-    }
-  })
+function sendTextMessages(sender, text) {
+  let messageData = {text: text[0]}
+  if (text.length === 0) {
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: token},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: messageData
+      }
+    }, function(error, response,body) {
+      if (error) {
+        console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+      }
+      text = text.slice(1)
+      sendTextMessages(sender, text)
+    })
+  }
 }
 
 function resToMorningRoutine(sender) {
@@ -85,21 +86,21 @@ function resToMorningRoutine(sender) {
         "attachment": {
             "type": "template",
             "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "text": "Awesome! Do you have a morning routine you'd like to stick to?",
-                    // "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "postback",
-                        // "url": "https://www.messenger.com",
-                        "payload": "yes",
-                        "title": "yes"
-                    }, {
-                        "type": "postback",
-                        "title": "no",
-                        "payload": "no",
-                    }]
-                }]
+                "template_type": "button",
+
+                "text": "Awesome! Do you have a morning routine you'd like to stick to?",
+
+                "buttons": [{
+                      "type": "postback",
+                      // "url": "https://www.messenger.com",
+                      "payload": "yes",
+                      "title": "yes"
+                  }, {
+                      "type": "postback",
+                      "title": "no",
+                      "payload": "no",
+                  }]
+
             }
         }
     }
